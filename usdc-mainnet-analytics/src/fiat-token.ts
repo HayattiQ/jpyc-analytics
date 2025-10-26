504648562f4ed66f0c0118d24f0da2a83803a0ae
HayattiQ <gurujowa@gmail.com>
2025-10-26 20:04:02 +0900

    add transfer subgraph method

--- usdc-mainnet-analytics/src/fiat-token.ts
+++ usdc-mainnet-analytics/src/fiat-token.ts
@@ -1,5 +1,32 @@
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
 import { Transfer as TransferEvent } from "../generated/FiatToken/FiatToken"
-import { Transfer } from "../generated/schema"
+import { Account, GlobalStat, Transfer } from "../generated/schema"
+
+const ZERO_ADDRESS = Address.zero()
+const ONE = BigInt.fromI32(1)
+
+function loadOrCreateAccount(address: Address): Account {
+  let account = Account.load(address)
+  if (account == null) {
+    account = new Account(address)
+    account.balance = BigInt.zero()
+    account.updatedAtBlock = BigInt.zero()
+    account.updatedAtTimestamp = BigInt.zero()
+  }
+  return account
+}
+
+function loadOrCreateGlobalStat(): GlobalStat {
+  let statId = Bytes.fromUTF8("global")
+  let stat = GlobalStat.load(statId)
+  if (stat == null) {
+    stat = new GlobalStat(statId)
+    stat.holderCount = BigInt.zero()
+    stat.updatedAtBlock = BigInt.zero()
+    stat.updatedAtTimestamp = BigInt.zero()
+  }
+  return stat
+}
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
@@ -14,4 +41,40 @@ export function handleTransfer(event: TransferEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let stat = loadOrCreateGlobalStat()
+
+  if (event.params.from.notEqual(ZERO_ADDRESS)) {
+    let fromAccount = loadOrCreateAccount(event.params.from)
+    let previousBalance = fromAccount.balance
+    fromAccount.balance = previousBalance.minus(event.params.value)
+    if (
+      previousBalance.gt(BigInt.zero()) &&
+      fromAccount.balance.equals(BigInt.zero())
+    ) {
+      stat.holderCount = stat.holderCount.minus(ONE)
+    }
+    fromAccount.updatedAtBlock = event.block.number
+    fromAccount.updatedAtTimestamp = event.block.timestamp
+    fromAccount.save()
+  }
+
+  if (event.params.to.notEqual(ZERO_ADDRESS)) {
+    let toAccount = loadOrCreateAccount(event.params.to)
+    let previousBalance = toAccount.balance
+    toAccount.balance = previousBalance.plus(event.params.value)
+    if (
+      previousBalance.equals(BigInt.zero()) &&
+      toAccount.balance.gt(BigInt.zero())
+    ) {
+      stat.holderCount = stat.holderCount.plus(ONE)
+    }
+    toAccount.updatedAtBlock = event.block.number
+    toAccount.updatedAtTimestamp = event.block.timestamp
+    toAccount.save()
+  }
+
+  stat.updatedAtBlock = event.block.number
+  stat.updatedAtTimestamp = event.block.timestamp
+  stat.save()
 }
--- usdc-mainnet-analytics/tests/fiat-token.test.ts
+++ usdc-mainnet-analytics/tests/fiat-token.test.ts
@@ -1,7 +1,9 @@
 import { assert, clearStore, describe, test, beforeEach, afterEach } from "matchstick-as/assembly/index"
-import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
 import { handleTransfer } from "../src/fiat-token"
 import { createTransferEvent } from "./fiat-token-utils"
+
+const ZERO_ADDRESS = Address.fromString("0x0000000000000000000000000000000000000000")
 
 describe("FiatToken transfer handler", () => {
   beforeEach(() => {
@@ -25,5 +27,26 @@ describe("FiatToken transfer handler", () => {
     assert.fieldEquals("Transfer", id, "from", from.toHexString())
     assert.fieldEquals("Transfer", id, "to", to.toHexString())
     assert.fieldEquals("Transfer", id, "value", value.toString())
+  })
+
+  test("updates holder count when balances cross zero boundary", () => {
+    let holder = Address.fromString("0x00000000000000000000000000000000000000c0")
+    let mintValue = BigInt.fromI32(500)
+    let burnValue = BigInt.fromI32(500)
+
+    // Mint to holder (from zero address)
+    let mintEvent = createTransferEvent(ZERO_ADDRESS, holder, mintValue)
+    handleTransfer(mintEvent)
+
+    assert.fieldEquals("Account", holder.toHexString(), "balance", mintValue.toString())
+    let statId = Bytes.fromUTF8("global").toHexString()
+    assert.fieldEquals("GlobalStat", statId, "holderCount", "1")
+
+    // Burn entire balance (to zero address)
+    let burnEvent = createTransferEvent(holder, ZERO_ADDRESS, burnValue)
+    handleTransfer(burnEvent)
+
+    assert.fieldEquals("Account", holder.toHexString(), "balance", "0")
+    assert.fieldEquals("GlobalStat", statId, "holderCount", "0")
   })
 })
